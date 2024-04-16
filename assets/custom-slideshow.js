10426e7efce64cb323fa3124b0bd33b659979771
vaibhav <vaibhavsindhe10@email.com>
2024-04-16 21:31:50 +0530

    adding custom-splideshow

--- assets/custom-slideshow.js
+++ assets/custom-slideshow.js
@@ -1,18 +1,17 @@
-class CustomSlider extends HTMLElement {
-    constructor() {
-      super();
-      this.element = this.querySelector(".splide");
-      this.options = JSON.parse(this.element.dataset.sliderSettings);
-      this.mountSplider()
+    class CustomSlider extends HTMLElement {
+      constructor() {
+        super();
+        this.element = this.querySelector(".splide");
+        this.options = JSON.parse(this.element.dataset.sliderSettings);
+        this.mountSplider()
+        console.log(this);
+      }
+    
+      mountSplider() {
+        new Splide(this.element,this.options).mount();
+      }
     }
+    customElements.define("splider-component", CustomSlider);
   
-    mountSplider() {
-      new Splide(this.element,this.options).mount();
-    }
-  }
   
-
-  customElements.define("splider-component", CustomSlider);
-
-
-  console.log("hello");
+  

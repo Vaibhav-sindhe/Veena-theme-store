2861b55180a804a0ce8a091e0b6c57c8f6c33578
vaibhav <vaibhavsindhe10@email.com>
2024-04-17 00:03:02 +0530

    adding letyou-know section

---
+++ assets/letyou-know.js
@@ -0,0 +1,16 @@
+class CustomSlider extends HTMLElement {
+    constructor() {
+      super();
+      this.element = this.querySelector(".splide");
+      this.options = JSON.parse(this.element.dataset.sliderSettings);
+      this.mountSplider()
+      console.log(this);
+    }
+  
+    mountSplider() {
+      new Splide(this.element,this.options).mount();
+    }
+  }
+  customElements.define("blog-component", CustomSlider);
+
+

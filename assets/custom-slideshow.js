7aa1426599913744488314656ef766cce5889f79
vaibhav <vaibhavsindhe10@email.com>
2024-04-16 15:57:22 +0530

    adding captivating collection

---
+++ assets/custom-slideshow.js
@@ -0,0 +1,15 @@
+{{ 'custom-slideshow.js' | asset_url | script_tag }}
+class CustomSlider extends HTMLElement {
+    constructor() {
+      super();
+      this.element = this.querySelector(".splide");
+      this.options = JSON.parse(this.element.dataset.sliderSettings);
+      this.mountSplider()
+    }
+  
+    mountSplider() {
+      new Splide(this.element,this.options).mount();
+    }
+  }
+  
+  customElements.define("splider-component", CustomSlider);

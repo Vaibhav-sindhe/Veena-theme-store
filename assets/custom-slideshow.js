a819d83a22b1b23613b31cbcd87a89d58d1978b9
vaibhav <vaibhavsindhe10@email.com>
2024-04-20 11:15:46 +0530

    week-6

--- assets/custom-slideshow.js
+++ assets/custom-slideshow.js
@@ -6,9 +6,29 @@
         this.mountSplider(this.element.querySelectorAll('.splide__slide'))
       }
     
-      mountSplider(slideList) {
-          splide=new Splide(this.element,this.options).mount();
-          slides=slideList;
+      mountSplider(slides) {
+        let splide=new Splide(this.element,this.options).mount();
+          var productImage = document.querySelector('.week-6-product img');
+          let prevSlide;
+          
+          function updateProductImage(slideIndex) {
+            if(prevSlide){
+              prevSlide.classList.remove("card-active");
+            }
+              var slideElement = slides[slideIndex];
+              slideElement.classList.add("card-active");
+              prevSlide=slideElement;
+              var productImageURL = slideElement.dataset.productImg;
+              console.log(slideElement);
+              productImage.src = productImageURL;
+          }
+          
+        
+        splide.on('moved', function (newIndex) {
+            if (!isNaN(newIndex) && newIndex >= 0 && newIndex < slides.length) {
+                updateProductImage(newIndex);
+            }
+        });
       }
     }
     customElements.define("splider-component", CustomSlider);

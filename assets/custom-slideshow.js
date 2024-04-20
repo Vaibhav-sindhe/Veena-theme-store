8e8b4f9251223a5cce425cc950b4bc97157fe4ce
vaibhav <vaibhavsindhe10@email.com>
2024-04-20 11:23:39 +0530

    week-6

--- assets/custom-slideshow.js
+++ assets/custom-slideshow.js
@@ -5,37 +5,32 @@
         this.options = JSON.parse(this.element.dataset.sliderSettings);
         this.mountSplider(this.element.querySelectorAll('.splide__slide'))
       }
-    }
- let splide;
- let prevSlide;
- var productImage = document.querySelector('.week-6-product img');        
-  
- 
- function mountSplider(slides) {
-       splide=new Splide(this.element,this.options).mount();
-
-
-        function updateProductImage(slideIndex) {
-          console.log("updated")
-          if(prevSlide){
-            prevSlide.classList.remove("card-active");
+    
+      mountSplider(slides) {
+        let splide=new Splide(this.element,this.options).mount();
+          var productImage = document.querySelector('.week-6-product img');
+          let prevSlide;
+          
+          function updateProductImage(slideIndex) {
+            if(prevSlide){
+              prevSlide.classList.remove("card-active");
+            }
+              var slideElement = slides[slideIndex];
+              slideElement.classList.add("card-active");
+              prevSlide=slideElement;
+              var productImageURL = slideElement.dataset.productImg;
+              console.log(slideElement);
+              productImage.src = productImageURL;
           }
-            var slideElement = slides[slideIndex];
-            slideElement.classList.add("card-active");
-            prevSlide=slideElement;
-            var productImageURL = slideElement.dataset.productImg;
-            console.log(slideElement);
-            productImage.src = productImageURL;
-        }
+          
         
-      
-      splide.on('moved', function (newIndex) {
-        console.log("moved");
-          if (!isNaN(newIndex) && newIndex >= 0 && newIndex < slides.length) {
-              updateProductImage(newIndex);
-          }
-      });
-    }    
+        splide.on('moved', function (newIndex) {
+            if (!isNaN(newIndex) && newIndex >= 0 && newIndex < slides.length) {
+                updateProductImage(newIndex);
+            }
+        });
+      }
+    }
     customElements.define("splider-component", CustomSlider);
   
   

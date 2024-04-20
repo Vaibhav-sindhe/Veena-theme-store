86c605def68fa7abb692482c29fb1d3992806a2b
vaibhav <vaibhavsindhe10@email.com>
2024-04-20 10:49:01 +0530

    week-6

--- assets/custom-slideshow.js
+++ assets/custom-slideshow.js
@@ -9,23 +9,19 @@
     
       mountSplider(slides) {
         let splide=new Splide(this.element,this.options).mount();
-          var blocks = blocksData;
           var productImage = document.querySelector('.week-6-product img');
           
           
           function updateProductImage(slideIndex) {
               var slideElement = slides[slideIndex];
               var productImageURL = slideElement.dataset.productImg;
-              console.log(productImageURL);
+              console.log(slideElement);
               productImage.src = productImageURL;
           }
           
         
-        // Listen for slide change event
         splide.on('moved', function (newIndex) {
-            // Ensure that newIndex is a valid index
             if (!isNaN(newIndex) && newIndex >= 0 && newIndex < slides.length) {
-                // Update the product image based on the current slide index
                 updateProductImage(newIndex);
             }
         });

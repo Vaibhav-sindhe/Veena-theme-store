c7151886c993bf51b293734fe1f9974073984854
vaibhav <vaibhavsindhe10@email.com>
2024-04-23 18:27:14 +0530

    product-card

--- assets/custom-product-card.js
+++ assets/custom-product-card.js
@@ -1,56 +1,44 @@
-class MarmetoProductCard extends HTMLElement {
+class ProductCard extends HTMLElement {
     constructor() {
       super();
-      this.addEventListener('change', event => {
-        if(event.target.name == "id") this.variantChange();
-      })
-      this.addEventListener('click', event => {
-        if(event.target.name == "add") this.addToBag();
-      })
+      this.productHandle = this.dataset.productHandle;
+      this.sectionId = this.dataset.sectionId;
+      if (this.querySelector("script")) {
+        this.variantData = JSON.parse(this.querySelector("script").textContent);
+      }
+      this.addEventListener("change", this.onOptionChange);
     }
-    variantChange() {
-      
-      this.selectedVariantId = this.querySelector('[name="id"]').value;
-      let variantUrl = `/products/${this.dataset.productHandle}?view=card&variant=${this.selectedVariantId}`;
-       
-      //Change the DOM Elements
-      fetch(variantUrl)
+    onOptionChange() {
+      console.log("clicked");
+      console.log("product-handle", this.productHandle);
+      console.log("variant data",this.variantData);
+      this.selectedOptions = Array.from(
+        this.querySelectorAll('input[type="radio"]:checked'),
+        (input) => input.value
+      );
+      console.log("selectedoption",this.selectedOptions)
+      this.currentVariant = this.variantData.find(
+        (item) =>
+          JSON.stringify(item.options) == JSON.stringify(this.selectedOptions)
+      );
+      console.log("variantdata", this.variantData);
+      console.log("current variant", this.currentVariant);
+      this.getUpdatedCard();
+    }
+    getUpdatedCard() {
+    //   const url = `/products/${this.productHandle}?variant=${this.currentVariant.id}&section_id=custom-product-card`;
+      const url = `/products/${this.productHandle}?view=product-card&variant=${this.currentVariant.id}`;
+      console.log(url);
+      fetch(url)
         .then((response) => response.text())
         .then((responseText) => {
-          const html = new DOMParser().parseFromString(responseText, 'text/html');
-          const responseCard = html.querySelector('marmeto-product-card');
-  
-          this.innerHTML = responseCard.innerHTML;
+          const html = new DOMParser().parseFromString(responseText, "text/html");
+          console.log("html",html);
+          // this.innerHTML = html.querySelector(
+          //   `[data-product-handle="${this.productHandle}"]`
+          // ).innerHTML;
+          this.innerHTML = html.querySelector('product-card').innerHTML;
         });
     }
-    addToBag() {
-      this.selectedVariantId = this.querySelector('[name="id"]').value;
-  
-      let formData = {
-       'items': [{
-        'id': this.selectedVariantId,
-        'quantity': 1
-        }]
-      };
-      fetch(window.Shopify.routes.root + 'cart/add.js', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      })
-      .then(response => {
-        return response.json();
-      })
-      .then(response => {
-        //Replace the below line with ajax cart open and update code
-        document.location.href = '/cart';
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    }
   }
-  customElements.define('marmeto-product-card', MarmetoProductCard);
-  
-  
+  customElements.define("product-card", ProductCard);
